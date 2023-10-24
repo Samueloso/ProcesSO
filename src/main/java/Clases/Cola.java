4a6ab6e405cf362b42258bbc2e312e12c87c8314
samueloso <samuel.cedeno@correo.unimet.edu.ve>
2023-10-23 22:38:37 -0400

    hago primitivas

---
+++ src/main/java/Clases/Cola.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author samue
+ */
+public class Cola {
+    
+    private Nodo inicioCola;
+    private Nodo finCola; 
+    
+    public Cola(){
+    
+        inicioCola=null;
+        finCola=null; 
+        
+    }
+    
+    public boolean ColaVacia(){
+    
+        if (inicioCola == null){
+        return true;
+        }
+        else{
+        return false;
+        }
+    
+    }
+    
+    public void encolar(int dato){
+    
+        Nodo nuevoNodo = new Nodo(dato);
+        if (ColaVacia()){
+            inicioCola = nuevoNodo;
+            finCola = nuevoNodo;
+        }
+        else{
+        
+            finCola.setSiguiente(nuevoNodo);
+            finCola=nuevoNodo; 
+        
+        }
+    
+        
+    
+    }
+    
+    public int desecolar(){
+        
+        if(!ColaVacia()){
+        
+            int dato = inicioCola.getDato() ;
+            
+            if (inicioCola == finCola ){
+                inicioCola= null;
+                finCola= null;
+                
+            }
+            else{
+                inicioCola = inicioCola.getSiguiente();
+            }
+            return dato;
+            
+            
+        }
+        else{
+        return 0;
+        }
+        
+    }
+    
+    public void imprimirCola(){
+        
+        Nodo temporal = inicioCola; 
+        
+        System.out.println("Cola:");
+        
+        while (temporal!=null){
+            
+            System.out.println(temporal.getDato() + " ");
+            temporal =  temporal.getSiguiente();
+        
+        }
+        System.out.println("");
+    }
+    
+}
---
+++ src/main/java/Clases/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author samue
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Cola cola = new Cola();
+        cola.encolar(10);
+        cola.encolar(20);
+        cola.encolar(30);
+        
+        cola.imprimirCola();
+        
+        
+    }
+    
+}
