9f5bb8e2b98e531ad147024abd65f0cfbc689725
Radenixlol <ranseartin@gmail.com>
2023-11-18 17:44:06 -0400

    Terminación del método select y work

--- src/main/java/Clases/Admin.java
+++ src/main/java/Clases/Admin.java
@@ -4,7 +4,11 @@
  */
 package Clases;
 
+import static java.lang.Thread.sleep;
 import java.util.Random;
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -12,6 +16,9 @@ import java.util.Random;
  */
 public class Admin extends Thread {
 
+    private Semaphore sema1;
+    private Semaphore sema2;
+    private Semaphore sema3;
     private Cola Zcola1;
     private Cola Zcola2;
     private Cola Zcola3;
@@ -27,6 +34,9 @@ public class Admin extends Thread {
     private int ciclos;
 
     public Admin() {
+        this.sema1 = new Semaphore(1);
+        this.sema2 = new Semaphore(0);
+        this.sema3 = new Semaphore(1);
         this.Zcola1 = new Cola();
         this.Zcola2 = new Cola();
         this.Zcola3 = new Cola();
@@ -37,46 +47,131 @@ public class Admin extends Thread {
         this.SFcolar = new Cola();
         this.Zelda = new Personajes[10];
         this.StreetFighter = new Personajes[10];
-        this.Simulador = new IA();
+        this.Simulador = new IA(this.sema1, this.sema2, this.sema3);
         this.timing = 10;
         this.ciclos = 0;
+    }
+
+    public void init() {
+
+    }
+
+    @Override
+    public void run() {
+        init();
+        while (true) {
+            try {
+
+                Work();
+
+                sleep(timing*1000);
+
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Admin.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+    }
+
+    public void Work() {
+        Check();
+        try {
+            sema3.acquire(1);
+            sema1.acquire(1);
+            Select();
+            sema1.release();
+            sema2.release();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Admin.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public void Select() {
         Random rand = new Random();
         if (rand.nextDouble() <= 0.8) {
-            boolean foundZ = false;
-            boolean foundSF = false;
-            int i = 0;
-            while (!foundZ && !foundSF) {
-                if (!foundZ) {
-                    Personajes temp = Zelda[i];
-                    if (!temp.isActivate()) {
-                        SelectCola(temp, true);
-                        foundZ = true;
-                    }
-                }
-                if (!foundSF) {
-                    Personajes temp = StreetFighter[i];
-                    if (!temp.isActivate()) {
-                        SelectCola(temp, false);
-                        foundSF = true;
-                    }
-                }
-                i++;
-                if (i >= 10) {
-                    foundZ = true;
-                    foundSF = true;
+//            boolean foundZ = false;
+//            boolean foundSF = false;
+//            int i = 0;
+//            while (!foundZ && !foundSF) {
+//                if (!foundZ) {
+//                    Personajes temp = Zelda[i];
+//                    if (!temp.isActivate()) {
+//                        SelectCola(temp, true);
+//                        foundZ = true;
+//                    }
+//                }
+//                if (!foundSF) {
+//                    Personajes temp = StreetFighter[i];
+//                    if (!temp.isActivate()) {
+//                        SelectCola(temp, false);
+//                        foundSF = true;
+//                    }
+//                }
+//                i++;
+//                if (i >= 10) {
+//                    foundZ = true;
+//                    foundSF = true;
+//                }
+//            }
+        }
+        switch (1) {
+            case 1: {
+                if (!Zcola1.ColaVacia()) {
+                    Personajes temp = Zcola1.getInicioCola();
+                    Zcola1.desencolar_cabeza();
+                    //Función de añadir a la casilla Zelda
+                    break;
                 }
             }
+            case 2: {
+                if (!Zcola2.ColaVacia()) {
+                    Personajes temp = Zcola2.getInicioCola();
+                    Zcola2.desencolar_cabeza();
+                    //Función de añadir a la casilla Zelda
+                    break;
+                }
+            }
+            case 3: {
+                if (!Zcola3.ColaVacia()) {
+                    Personajes temp = Zcola3.getInicioCola();
+                    Zcola3.desencolar_cabeza();
+                    //Función de añadir a la casilla Zelda
+                    break;
+                }
+            }
+            default: {
+                System.out.println("No hay personajes en cola");
+            }
         }
-        boolean Zready = false;
-        boolean SFready = false;
-        while(!Zready){
-            
-        }
-        while(!SFready){
-            
+
+        switch (1) {
+            case 1: {
+                if (!SFcola1.ColaVacia()) {
+                    Personajes temp = SFcola1.getInicioCola();
+                    SFcola1.desencolar_cabeza();
+                    //Función de añadir a la casilla Street Fighter
+                    break;
+                }
+            }
+            case 2: {
+                if (!SFcola2.ColaVacia()) {
+                    Personajes temp = SFcola2.getInicioCola();
+                    SFcola2.desencolar_cabeza();
+                    //Función de añadir a la casilla Street Fighter
+                    break;
+                }
+            }
+            case 3: {
+                if (!SFcola3.ColaVacia()) {
+                    Personajes temp = SFcola3.getInicioCola();
+                    SFcola3.desencolar_cabeza();
+                    //Función de añadir a la casilla Street Fighter
+                    break;
+                }
+            }
+            default: {
+                System.out.println("No hay personajes en cola");
+            }
         }
     }
 
--- src/main/java/Clases/IA.java
+++ src/main/java/Clases/IA.java
@@ -4,13 +4,26 @@
  */
 package Clases;
 
+import java.util.concurrent.Semaphore;
+
 /**
  *
  * @author Radenixlol
  */
 public class IA {
-
-    public IA() {
+    
+    private Semaphore sema1;
+    private Semaphore sema2;
+    private Semaphore sema3;
+    private Personajes Z;
+    private Personajes SF;
+    
+    public IA(Semaphore sema1, Semaphore sema2, Semaphore sema3) {
+        this.sema1 = sema1;
+        this.sema2 = sema2;
+        this.sema3 = sema3;
+        this.Z = null;
+        this.SF = null;
     }
     
 }
